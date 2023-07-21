fff6284c4196d37b0d655e9ecf9fe039dbf73c79
Yahuza Nuhu <87288867+nuhuyahuza@users.noreply.github.com>
2023-07-21 14:11:01 +0000

    second changes

---
+++ src/content_option.js
@@ -0,0 +1,181 @@
+const logotext = "Shadow";
+const meta = {
+  title: "Nuhu Yahuza",
+  description:
+    "I’m Nuhu Yahuza  Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Nuhu Yahuza",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "I am a passionate and skilled Full Stack Developer with a diverse range of experience in designing and building robust web applications. Through my journey in the world of programming, I have honed my expertise in both front-end and back-end technologies.",
+  your_img_url: "/images/profile2.png",
+};
+
+const dataabout = {
+  title: "a bit about my self",
+  aboutme:
+    "I am a passionate and skilled Full Stack Developer with a diverse range of experience in designing and building robust web applications. Through my journey in the world of programming, I have honed my expertise in both front-end and back-end technologies.On the front-end, I specialize in creating captivating user interfaces using HTML, CSS, and JavaScript, while implementing responsive designs to ensure seamless user experiences across all devices.When it comes to the back-end, I am well-versed in handling server-side logic and databases. I enjoy working with technologies like Node.js, Express, and MongoDB, crafting efficient and scalable solutions to power the core functionality of web applications.Throughout my projects, I strive for clean code architecture, adherence to best practices, and a focus on performance and security.Feel free to explore my portfolio to see some of my recent projects and the skills I bring to the table. If you have any inquiries or collaboration opportunities, please don't hesitate to get in touch with me through the contact information provided. Thank you for visiting, and I hope you find my work inspiring!",
+};
+const worktimeline = [
+  {
+    jobtitle: "Software Developer",
+    where: "BoldTech Ghana",
+    date: "2022-2023",
+  },
+  {
+    jobtitle: "UI Developer",
+    where: "Nhyera's Studios",
+    date: "2021",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "PHP",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "nuhuyahuza@yahoo.com",
+  YOUR_FONE: "(024)288-0401",
+  description: "Send me a message and i will get back to you soon",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/nuhuyahuza",
+  linkedin: "https://linkedin.com/in/nuhu-yahuza-533360215",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
-import withRouter from "../hooks/withRouter"
-import { Home } from "../pages/home";
-import { Portfolio } from "../pages/portfolio";
-import { ContactUs } from "../pages/contact";
-import { About } from "../pages/about";
-import { Socialicons } from "../components/socialicons";
+import { Route, Routes } from "react-router-dom";
+import withRouter from "./hooks/withRouter";
+import { Home } from "./pages/home";
+import { Portfolio } from "./pages/portfolio";
+import { ContactUs } from "./pages/contact";
+import { About } from "./pages/about";
+import { Socialicons } from "./components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const AnimatedRoutes = withRouter(({ location }) => (
